aaf36d8ee367ad2e3b69e4c1208bf514608371f6
Chaimaazerouali <zeroualiiichaimaa@gmail.com>
2023-12-04 18:34:25 -0800

    python

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""
-Defines a MyInt class that inverts the behavior of '==' and '!=' for int objects.
-"""
+""" inverts eq and nq in a class """
+
 
 class MyInt(int):
-    """Inverted equality for the int class."""
+    """ inverted equality for int class """
 
     def __eq__(self, other):
-        """Overrides the '==' operator to return False when objects are of the same type."""
         if isinstance(self, type(other)):
             return False
 
     def __ne__(self, other):
-        """Overrides the '!=' operator to return True when objects are of the same type."""
         if isinstance(self, type(other)):
             return True
-
