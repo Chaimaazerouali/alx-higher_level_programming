6dbaf4c01befa599905d4b1a198d3d4c09bbbded
Chaimaazerouali <zeroualiiichaimaaa@gmail.com>
2024-02-18 14:47:32 -0800

    alx

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""creates model CIty, if error, delete unique on id"""
-
-from relationship_state import Base, State
+"""
+Contains the class definition of a City
+"""
+from relationship_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(Base):
-    """ create city class base """
+    """
+    Class that defines each city
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer,
-                      ForeignKey('states.id'),
-                      nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
