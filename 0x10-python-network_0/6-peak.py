8a7358beee68559091dd06e717148a545871e22c
Chaimaazerouali <zeroualiiichaimaaa@gmail.com>
2024-02-29 16:58:56 -0800

    alx

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
+""" find min number of a list """
 
 
 def find_peak(list_of_integers):
-"""Find a peak in list_of_integers"""
+    """Finds a peak in list_of_integers"""
 
-if list_of_integers is None or list_of_integers == []:
-return None
-low = 0
-high = len(list_of_integers)
-mid = ((high - low) // 2) + low
-mid = int(mid)
-if high == 1:
-return list_of_integers[0]
-if high == 2:
-return max(list_of_integers)
-if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-list_of_integers[mid] >= list_of_integers[mid + 1]:
-return list_of_integers[mid]
-if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-return find_peak(list_of_integers[mid:])
-if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-return find_peak(list_of_integers[:mid])
-
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    lw = 0
+    hgt = len(list_of_integers)
+    moy = ((hgt - lw) // 2) + lw
+    moy = int(moy)
+    if hgt == 1:
+        return list_of_integers[0]
+    if hgt == 2:
+        return max(list_of_integers)
+    if list_of_integers[moy] >= list_of_integers[moy - 1] and\
+            list_of_integers[moy] >= list_of_integers[moy + 1]:
+        return list_of_integers[moy]
+    if moy > 0 and list_of_integers[moy] < list_of_integers[moy + 1]:
+        return find_peak(list_of_integers[moy:])
+    if moy > 0 and list_of_integers[moy] < list_of_integers[moy - 1]:
+        return find_peak(list_of_integers[:moy])
