24cd1448ef1dccc0d76074bed814faee4b92381d
Chaimaazerouali <zeroualiiichaimaaa@gmail.com>
2024-03-01 17:35:48 -0800

    alx

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-""" Write a Python script that shows
-    X-Request_Id Header of an URL """
+"""Python script to:
+
+- accept a URL input,
+- make a request to the URL,
+- extract and display the value
+- of the X-Request-Id variable
+- from the response header.
+"""
 
 import urllib.request
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+url = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+request = urllib.request.Request(url)
+with urllib.request.urlopen(request) as response:
+print(dict(response.headers).get("X-Request-Id"))
 
