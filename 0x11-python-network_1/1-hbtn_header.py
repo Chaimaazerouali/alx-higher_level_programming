248a5413f976a6787b569d3c2033a42c66f10725
Chaimaazerouali <zeroualiiichaimaaa@gmail.com>
2024-03-01 17:20:39 -0800

    alx

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id Header of an URL.
+"""
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+with urllib.request.urlopen(sys.argv[1]) as response:
+print(response.info().get('X-Request-Id'))
+
